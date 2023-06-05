7fea17352fcc66519f510f4045b4de78ca0e9f5c
Srajan <94606073+srajan-06@users.noreply.github.com>
2023-06-05 17:18:18 +0530

    Create First Missing Positive.cpp
    Given an unsorted integer array nums, return the smallest missing positive integer.
    
    You must implement an algorithm that runs in O(n) time and uses constant extra space.
    
     
    
    Example 1:
    
    Input: nums = [1,2,0]
    Output: 3
    Explanation: The numbers in the range [1,2] are all in the array.
    Example 2:
    
    Input: nums = [3,4,-1,1]
    Output: 2
    Explanation: 1 is in the array but 2 is missing.
    Example 3:
    
    Input: nums = [7,8,9,11,12]
    Output: 1
    Explanation: The smallest positive integer 1 is missing.
     
    
    Constraints:
    
    1 <= nums.length <= 105
    -231 <= nums[i] <= 231 - 1

---
+++ Hard/First Missing Positive.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+         int size = nums.size();
+        vector<int> sequence(size, -1);
+
+        for(int i=0;i<size;i++){
+            if(nums[i]>0 && nums[i]<=size){
+                sequence[nums[i]-1] = 1;
+            }
+        }
+
+        for(int i=0;i<size;i++){
+            if(sequence[i] == -1){
+                return i+1;
+            }
+        }
+        return size+1;       
+    }
+};
