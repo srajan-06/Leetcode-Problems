c461031f328f75e946911738729f5e370f7137ee
Srajan <94606073+srajan-06@users.noreply.github.com>
2023-06-05 17:25:38 +0530

    Create Spiral Matrix II.cpp
    Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
    
     
    
    Example 1:
    
    
    Input: n = 3
    Output: [[1,2,3],[8,9,4],[7,6,5]]
    Example 2:
    
    Input: n = 1
    Output: [[1]]
     
    
    Constraints:
    
    1 <= n <= 20

---
+++ Medium/Spiral Matrix II.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int arr[][] = new int[n][n];
+        int r1=0, r2=n-1, c1=0, c2=n-1;
+        int val = 1;
+
+        while(r1<=r2 && c1<=c2){
+            for(int i=c1;i<=c2;i++){
+                arr[r1][i] = val++;
+            }
+
+            for(int i=r1+1;i<=r2;i++){
+                arr[i][c2] = val++;
+            }
+
+            if(r1<r2 && c1<c2){
+                for(int i=c2-1;i>=c1;i--){
+                    arr[r2][i] = val++;
+                }
+
+                for(int i=r2-1;i>r1;i--){
+                    arr[i][c1] = val++;
+                }
+            }
+            r1++; r2--;
+            c1++; c2--;
+        }
+        return arr;
+        
+    }
+    
+}
